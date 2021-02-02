635e2b994f89e7c0ba849f34c51a1d7cd12b22c5
Pralhad322 <pralhadmagadum322@gmail.com>
2021-02-02 21:23:03 +0530

    comment

--- test/run.sh
+++ test/run.sh
@@ -4,8 +4,8 @@ gcc -fPIC -c trans.c
 gcc -shared -o libtrans.so trans.o
 gcc -L$(pwd) -Wl,-rpath=$(pwd) -o test main.c -ltrans -g
 
-cp trans.h ../thirdparty/linux/include/
-cp libtrans.so ../thirdparty/linux/lib/x64/
+#cp trans.h ../thirdparty/linux/include/
+#cp libtrans.so ../thirdparty/linux/lib/x64/
 
 #cp mul.h thirdparty/Mac/include/
 #cp libmul.so thirdparty/Mac/lib/x64/
